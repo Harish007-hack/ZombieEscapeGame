a5b59162717e0fd72414833aed1586fa0807f00d
RageGaming-LLP <g7harish@gmail.com>
2023-08-02 17:19:34 +0530

    Updates: Zombie Character start

---
+++ Source/ZombieEscape/ZombieCharacter.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZombieCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+// Sets default values
+AZombieCharacter::AZombieCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	GetCapsuleComponent()->InitCapsuleSize(42.f,96.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->MaxWalkSpeed = 700.f;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->JumpZVelocity = 0.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 700.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 350.f;
+
+
+
+}
+
+// Called when the game starts or when spawned
+void AZombieCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(APlayerController* PlayerController = Cast<APlayerController>(GetController())){
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())){
+			Subsystem->AddMappingContext(MappingContext,0);
+		}
+	}
+	
+}
+
+// Called every frame
+void AZombieCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AZombieCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)){
+
+		EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&AZombieCharacter::Move);
+
+		EnhancedInputComponent->BindAction(LookAction,ETriggerEvent::Triggered,this,&AZombieCharacter::Look);
+	}
+
+}
+
+void AZombieCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if(GetController() != nullptr){
+
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0.f,Rotation.Yaw,0.f);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+
+		AddMovementInput(ForwardDirection,MovementVector.Y);
+		AddMovementInput(RightDirection,MovementVector.X); 
+
+	}
+}
+
+void AZombieCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookDirectionVector = Value.Get<FVector2D>();
+
+	if(GetController() != nullptr){
+		AddControllerYawInput(LookDirectionVector.X);
+		AddControllerPitchInput(LookDirectionVector.Y);
+	}
+}
+
